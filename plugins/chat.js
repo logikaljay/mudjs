419da1b829db5f8ec8a883d279c7181fb660521b
Jay <jay@efinity.co.nz>
2015-01-08 10:11:38 +1300

    working on chat protocol

--- plugins/chat.js
+++ plugins/chat.js
@@ -1,6 +1,7 @@
 var telnet = require('../lib/telnet');
 var chat = exports = module.exports = {};
 
+
 chat.connection = new telnet();
 chat.initialized = false;
 
@@ -8,6 +9,17 @@ chat.load = function(mudjs) {
     console.log("[CHAT] Plugin loaded");
 
     this.connect('localhost', '4051');
+	
+	// init commands
+	mudjs._commands.chat = function(mudjs, args) {
+        if (args.length >= 2) {
+	        if (chat.connection !== null || chat.connection !== undefined) {
+                chat.connection.write('5\n' + args[0] + '\n' + args[1]);
+            }
+        } else {
+            console.log('Usage: /chat [name] [text]');
+        }
+	};
 }
 chat.unload = function() {
     console.log("[CHAT] Plugin unloaded");
@@ -20,23 +32,22 @@ chat.connect = function(host, port) {
     });
 
     this.connection.on('data', function(data) {
-	var str = data.toString();
+        console.log(data.length);
+        var str = data.toString();
         if (str.indexOf('YES:') > -1) {
+		    
             var chatName = str.replace('YES:', '');
             
             // to complete the handshake, send our version
-            chat.connection.write('19\nmudjs v0.1\n255');
-            //chat.connection.write('123');
-        }
-
-	console.log('data received');
-        process.stdout.write(data);
-        //process.stdout.write('\n');
+            chat.connection.write('19\nmudjs v0.1\n');
+        } else {
+            process.stdout.write(data);
+		}
     });
 
     this.connection.on('connect', function() {
-        console.log("connected to chat");
-	chat.connection.write('CHAT:jay\n10.1.1.1\n4050');
+        console.log('Connected - Waiting for response');
+	    chat.connection.write('CHAT:kanato\n10.1.1.1\n4050');
     });
 
     this.connection.on('error', function(err) {
