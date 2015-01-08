cdba4155a72789932db7d5df7ec8442cc5f2fb42
Jay <jay@efinity.co.nz>
2015-01-08 14:44:07 +1300

    still attempting to get chat to work

--- plugins/chat.js
+++ plugins/chat.js
@@ -1,24 +1,34 @@
 var telnet = require('../lib/telnet');
 var chat = exports = module.exports = {};
-
+var util = require('util');
 
 chat.connection = new telnet();
 chat.initialized = false;
-
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
 chat.load = function(mudjs) {
     console.log("[CHAT] Plugin loaded");
 
-    this.connect('localhost', '4051');
+    this.connect('10.1.1.36', '4050');
 	
 	// init commands
 	mudjs._commands.chat = function(mudjs, args) {
-        if (args.length >= 2) {
-	        if (chat.connection !== null || chat.connection !== undefined) {
-                chat.connection.write('5\n' + args[0] + '\n' + args[1]);
-            }
-        } else {
-            console.log('Usage: /chat [name] [text]');
-        }
+        if (chat.connection !== undefined) {
+			var data = convertToHex("\033[0;1;31mkanato chats to everyone, " + "'" + args.join(" ") + "'");
+			var buf = new Buffer('04'+data+'0A', 'hex');
+			console.log(chat);
+			chat.connection.write('4foo255'); 
+			console.log("written successfully");
+			console.log(buf);
+			
+		} else {
+			console.log('you are not connected to anyone');
+		}
 	};
 }
 chat.unload = function() {
@@ -32,22 +42,21 @@ chat.connect = function(host, port) {
     });
 
     this.connection.on('data', function(data) {
-        console.log(data.length);
         var str = data.toString();
         if (str.indexOf('YES:') > -1) {
-		    
             var chatName = str.replace('YES:', '');
             
             // to complete the handshake, send our version
-            chat.connection.write('19\nmudjs v0.1\n');
+            chat.connection.write('19\nmudjs v0.1\n255');
         } else {
+			console.log(data);
             process.stdout.write(data);
 		}
     });
 
     this.connection.on('connect', function() {
         console.log('Connected - Waiting for response');
-	    chat.connection.write('CHAT:kanato\n10.1.1.1\n4050');
+	    chat.connection.write('CHAT:kanato\n10.1.1.14050 ');
     });
 
     this.connection.on('error', function(err) {
