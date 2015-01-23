ebf5ca297f447d124f3d29a6f58e99365b750fcb
Jay <logikal@gmail.com>
2015-01-24 11:20:35 +1300

    adding code to enable/disable triggers
    needs testing

--- commands/cmd_groupdisable.js
+++ commands/cmd_groupdisable.js
@@ -6,7 +6,16 @@ var cmd_groupdisable = (function() {
         description: 'Name of the group to disable',
         args: [],
         init: function(mudjs, args) {
-            process.exit(0);
+            var group = args[0];
+
+            // itreate over all triggers
+            for (var i in mudjs._triggers) {
+                var trigger = mudjs._triggers[i];
+
+                if (trigger.group == args[0]) {
+                    trigger.enabled = false;
+                }
+            }
         }
     };
 
--- commands/cmd_groupenable.js
+++ commands/cmd_groupenable.js
@@ -6,7 +6,16 @@ var cmd_groupenable = (function() {
         description: 'Name of the group to enable',
         args: [],
         init: function(mudjs, args) {
-            process.exit(0);
+            var group = args[0];
+
+            // itreate over all triggers
+            for (var i in mudjs._triggers) {
+                var trigger = mudjs._triggers[i];
+
+                if (trigger.group == args[0]) {
+                    trigger.enabled = true;
+                }
+            }
         }
     };
 
