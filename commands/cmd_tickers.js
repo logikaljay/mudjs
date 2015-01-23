d445654033fa8b513afe6ab2e26f340584ee7a8f
Jay <logikal@gmail.com>
2015-01-24 12:09:30 +1300

    removing erronous code from tickers command

--- commands/cmd_tickers.js
+++ commands/cmd_tickers.js
@@ -6,21 +6,7 @@ var cmd_tickers = (function() {
         description: 'Lists all of the current running tickers',
         args: [],
         init: function(mudjs, args) {
-            if (args.length < 2) {
-                mudjs.showme('Usage: /ticker [x] [command]')
-                return;
-            }
-
-            var interval = parseInt(args.shift());
-            var command = args.join(' ');
-
-            var ticker = setInterval(
-                function() {
-                    mudjs.sendCommand(command);
-                }, (interval * 1000)
-            );
-
-            mudjs._tickers.push(ticker);
+            
         }
     };
 
