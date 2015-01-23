9edf1bad139908fd41c9eb36562f859008deaac6
Jay <logikal@gmail.com>
2015-01-24 12:09:14 +1300

    working on stopTicker

--- commands/cmd_stopTicker.js
+++ commands/cmd_stopTicker.js
@@ -6,12 +6,27 @@ var cmd_stopTicker = (function() {
         description: 'Stops and clears the ticker',
         args: [
         {
-            name: 'tickerName',
-            description: 'Name of the ticker you want to stop',
+            name: 'number',
+            description: 'The number of the ticker to stop',
             optional: false
-        }],
+        }
+        ],
         init: function(mudjs, args) {
-            
+            if (args.length < 1) {
+                mudjs.showme('Usage: /stopTicker [#]')
+                return;
+            }
+
+            var interval = parseInt(args.shift());
+            var command = args.join(' ');
+
+            var ticker = setInterval(
+                function() {
+                    mudjs.sendCommand(command);
+                }, (interval * 1000)
+            );
+
+            mudjs._tickers.push(ticker);
         }
     };
 
--- commands/cmd_tickers.js
+++ commands/cmd_tickers.js
@@ -6,7 +6,21 @@ var cmd_tickers = (function() {
         description: 'Lists all of the current running tickers',
         args: [],
         init: function(mudjs, args) {
-            
+            if (args.length < 2) {
+                mudjs.showme('Usage: /ticker [x] [command]')
+                return;
+            }
+
+            var interval = parseInt(args.shift());
+            var command = args.join(' ');
+
+            var ticker = setInterval(
+                function() {
+                    mudjs.sendCommand(command);
+                }, (interval * 1000)
+            );
+
+            mudjs._tickers.push(ticker);
         }
     };
 
