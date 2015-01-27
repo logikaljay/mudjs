f1262776124baaf59458a96e299bf76fa7dbf550
Jay <jay@efinity.co.nz>
2015-01-28 08:30:31 +1300

    added cli-table into the /help command

--- commands/cmd_help.js
+++ commands/cmd_help.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var Table = require('cli-table');
 
 var cmd_help = (function() {
     'use strict';
@@ -16,26 +17,44 @@ var cmd_help = (function() {
         init: function(mudjs, args) {
             // check if we are looking for specific help, or all help
             if (args.length == 0) {
+                var nameWidth = 0;
+                var table = new Table({
+                    style : {compact : true, 'padding-left' : 1},
+                    head: ['Name', 'Description']
+                });
+
                 // iterate over all commands, listing help
                 mudjs._commands._cmds.forEach(function(cmd) {
-                    mudjs.showme(util.format("/%s - %s", cmd.name, cmd.description));
+                    if (nameWidth.length < cmd.name.length) {
+                        nameWidth = cmd.name.length + 2;
+                    }
+
+                    table.push([cmd.name, cmd.description]);
                 });
+                mudjs.showme(table.toString());
+
             } else {
                 // find specific help
                 var cmd = mudjs._commands.find(args[0]);
                 if (cmd !== undefined) {
-                    var args = "";
-                    var argsDesc = "";
-                    cmd.args.forEach(function(arg) {
-                        args += util.format("[%s] ", arg.name);
-                        argsDesc += util.format("\t[%s] - %s %s\n",
-                            arg.name,
-                            arg.description,
-                            arg.optional ? "(optional)" : "");
+                    var cols = process.stdout.columns - 4;
+                    var nameWidth = args[0].length + 2;
+                    var descWidth = cols - nameWidth;
+
+                    var table = new Table({
+                        head: ['', 'Name', 'Description', '']
                     });
 
-                    mudjs.showme(util.format("/%s %s- %s", cmd.name, args, cmd.description));
-                    mudjs.showme(argsDesc)
+                    var args = "";
+                    table.push({ 'command': [ cmd.name, cmd.description, '']})
+                    cmd.args.forEach(function(arg) {
+                        args += util.format("[%s] ", arg.name);
+                        table.push({ 'param': [ arg.name, arg.description, arg.optional ? "(optional)" : "compulsory" ]})
+                    });
+
+                    //mudjs.showme(argsDesc)
+                    mudjs.showme(table.toString());
+                    mudjs.showme(util.format("EG: /%s %s- %s", cmd.name, args, cmd.description));
                 }
             }
         }
